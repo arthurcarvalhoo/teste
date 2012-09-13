8004dfb57f050bf7496b07ef38045c6d886e6aa2
Arthur Carvalho <arthurcarvalhoo@gmail.com>
2012-09-13 19:42:27 -0300

    aprendendo
    vamos ver se funciona

---
+++ src/com/example/tutorial/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.tutorial;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    
+}
